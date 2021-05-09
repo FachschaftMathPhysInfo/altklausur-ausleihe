c58de9b3bb675fa4107e122003386bc78bb0dc50
Christian Heusel <christian@heusel.eu>
2021-05-10 00:55:23 +0200

    basic watermarking

--- exam_marker/exam_marker.go
+++ exam_marker/exam_marker.go
@@ -1,14 +1,31 @@
 package main
 
 import (
-	"fmt"
 	"log"
 
-	"github.com/FachschaftMathPhysInfo/altklausur-ausleihe/exam_marker/v2/utils"
+	api "github.com/pdfcpu/pdfcpu/pkg/api"
+	"github.com/pdfcpu/pdfcpu/pkg/pdfcpu"
 )
 
+func applyWatermark() {
+	onTop := true
+	update := false
+
+	// Stamp all odd pages of the pfd in red at the right border of the document
+	wm, err := api.TextWatermark("test123 1. Mai 2021", "font:Courier, points:40, col: 1 0 0, rot:-90, sc:1 abs, opacity:0.4, pos: l, offset: -190 0", onTop, update, pdfcpu.POINTS)
+	api.AddWatermarksFile("in.pdf", "", []string{"odd"}, wm, nil)
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	// add the mathphys logo to the top-left corner? :P
+	wm, err = api.PDFWatermark("MathPhysLogoInfo.pdf", "pos:tr, rot:0, sc:0.5 abs, offset: -10 -10, opacity:0.5", onTop, update, pdfcpu.POINTS)
+	api.AddWatermarksFile("in.pdf", "", nil, wm, nil)
+	if err != nil {
+		log.Fatalln(err)
+	}
+}
+
 func main() {
-	fmt.Println("vim-go")
-	db := utils.InitDB()
-	log.Println(db)
+	applyWatermark()
 }
--- exam_marker/go.mod
+++ exam_marker/go.mod
@@ -6,6 +6,7 @@ require (
 	github.com/jackc/pgproto3/v2 v2.0.7 // indirect
 	github.com/lib/pq v1.6.0 // indirect
 	github.com/minio/minio-go/v7 v7.0.10
+	github.com/pdfcpu/pdfcpu v0.3.11 // indirect
 	golang.org/x/crypto v0.0.0-20210506145944-38f3c27a63bf // indirect
 	golang.org/x/text v0.3.6 // indirect
 	gorm.io/driver/postgres v1.1.0
