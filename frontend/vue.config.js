80e404ad591cd90b9246d1b7b11fc2f8858eaf34
Christian Heusel <christian@heusel.eu>
2022-12-08 21:08:51 +0100

    migration guide step 3
    Co-authored-by: Tom Rix <tom.rix@web.de>

--- frontend/vue.config.js
+++ frontend/vue.config.js
@@ -15,5 +15,23 @@ module.exports = {
       enableInSFC: true,
       enableBridge: false
     }
+  },
+
+  chainWebpack: (config) => {
+    config.resolve.alias.set('vue', '@vue/compat')
+
+    config.module
+      .rule('vue')
+      .use('vue-loader')
+      .tap((options) => {
+        return {
+          ...options,
+          compilerOptions: {
+            compatConfig: {
+              MODE: 2
+            }
+          }
+        }
+      })
   }
 }
